42a2fd7be53a65d81231cd08b92e4f189b55d09d
mohab2338 <mohab2338@gmail.com>
2026-09-20 13:01:30 +0300

    remove all

--- android_overlay/app/src/main/kotlin/com/example/salary_tracker/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.salary_tracker
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-// local_auth (fingerprint/biometric) requires a FragmentActivity host,
-// so this extends FlutterFragmentActivity rather than the default
-// FlutterActivity.
-class MainActivity : FlutterFragmentActivity()
